02459e23cb3782f53102ba7773b1dfc908eb8d5c
Niko Eklöf <80674961+nikoeklof@users.noreply.github.com>
2021-05-06 13:57:24 +0300

    ver 0.8
    more upgrades coming, saving and loading added

--- main.js
+++ main.js
@@ -2,18 +2,17 @@ var score = 0;
 var scorePerSecond = 0;
 var clickingPower = 1;
 var clickingPowerMultiplier = 0;
-var cursors = 0;
-var cursorValue = 1;
-var cursorCost = 10;
-var grannys = 0;
-var grannyValue = 3;
-var grannyCost = 250;
-var ovens = 0;
-var ovenValue = 8;
-var ovenCost = 5000;
-var factorys = 0;
-var factoryValue = 50;
-var factoryCost = 100000;
+//new version for all buyable objects, index 1 is for quantity, 2 for value, 3 for cost
+var cursor = [0, 0.1, 15]
+var oven = [0, 8, 1100]
+var granny = [0, 1, 100]
+var factory = [0, 47, 12000]
+    //upgrades, index 0 is quantity, 1 is multiplier, 2 is cost
+var cursorUpgrades = [0, 1, 100]
+var grannyUpgrades = [0, 1, 5000]
+var ovenUpgrades = [0, 1, 11000]
+var factoryUpgrades = [0, 1, 120000]
+
 
 function addScore(amount) {
     score = score + amount;
@@ -21,80 +20,243 @@ function addScore(amount) {
 }
 
 function buyCursor() {
-    if (score >= cursorCost) {
-        scorePerSecond += cursorValue * .75
-        clickingPowerMultiplier = (clickingPower / 15) + (cursorValue / 10)
-
-        clickingPower = clickingPower + (clickingPowerMultiplier / 5)
-        amount = cursorCost
-        cursors = cursors + 1
-        score = score - cursorCost
-        cursorCost = Math.round(cursorCost * 1.22)
+    let i = 0
+    if (score >= cursor[2]) {
+        scorePerSecond += cursor[1]
+        cursor[0] = cursor[0] + 1
+        score = score - cursor[2]
+        cursor[2] = Math.round(cursor[2] * 1.15)
         document.getElementById('score').innerHTML = score.toFixed(2)
-        document.getElementById('cursorCost').innerHTML = cursorCost
-        document.getElementById('cursors').innerHTML = cursors
-        document.getElementById('cps').innerHTML = scorePerSecond
+        document.getElementById('cursorCost').innerHTML = cursor[2]
+        document.getElementById('cursors').innerHTML = cursor[0]
+        document.getElementById('cps').innerHTML = scorePerSecond.toFixed(2)
         document.getElementById('cpc').innerHTML = clickingPower.toFixed(2)
+        if (cursorUpgrades[0] == 0 && cursor[0] == 15 && i == 0) {
+            document.getElementById('upgrades').innerHTML += '<button onclick="cursorUpgrade()">Upgrade cursors: <br>Cost: <span id="cursorUpgradeCost">100</span> </button>' +
+                '<span>Makes cursors and mouseclicks twice as efficient</span>'
+            i = 1
+        }
     }
+
 }
 
 function buyGranny() {
-
-    if (score >= grannyCost) {
-        scorePerSecond += grannyValue * .75
-        clickingPowerMultiplier = (clickingPower / 15) + (grannyValue / 10)
-
-        clickingPower = clickingPower + (clickingPowerMultiplier / 5)
-        amount = grannyCost
-        grannys = grannys + 1
-        score = score - grannyCost
-        grannyCost = Math.round(grannyCost * 1.22)
+    let i = 0
+    if (score >= granny[2]) {
+        scorePerSecond += granny[1]
+        granny[0] = granny[0] + 1
+        score = score - granny[2]
+        granny[2] = Math.round(granny[2] * 1.14)
         document.getElementById('score').innerHTML = score.toFixed(2)
-        document.getElementById('grannyCost').innerHTML = grannyCost
-        document.getElementById('grannys').innerHTML = grannys
-        document.getElementById('cps').innerHTML = scorePerSecond
+        document.getElementById('grannyCost').innerHTML = granny[2]
+        document.getElementById('grannys').innerHTML = granny[0]
+        document.getElementById('cps').innerHTML = scorePerSecond.toFixed(2)
         document.getElementById('cpc').innerHTML = clickingPower.toFixed(2)
-
+        if (grannyUpgrades[0] == 0 && granny[0] == 15 && i == 0) {
+            document.getElementById('upgrades').innerHTML += '<button onclick="grannyUpgrade()">Upgrade grannies: <br>Cost: <span id="grannyUpgradeCost">5000</span> </button>' +
+                '<span>Makes grannies twice as efficient</span>'
+            i = 1
+        }
     }
+
 }
 
 function buyOven() {
-    if (score >= ovenCost) {
-        scorePerSecond += ovenValue * .75
-        clickingPowerMultiplier = (clickingPower / 15) + (ovenValue / 10)
-        clickingPower = clickingPower + (clickingPowerMultiplier / 5)
-        amount = ovenCost
-        ovens = ovens + 1
-        score = score - ovenCost
-        ovenCost = Math.round(ovenCost * 1.22)
+    let i = 0
+    if (score >= oven[2]) {
+        scorePerSecond += oven[1]
+        oven[0] = oven[0] + 1
+        score = score - oven[2]
+        oven[2] = Math.round(oven[2] * 1.14)
         document.getElementById('score').innerHTML = score.toFixed(2)
-        document.getElementById('ovenCost').innerHTML = ovenCost
-        document.getElementById('ovens').innerHTML = ovens
-        document.getElementById('cps').innerHTML = scorePerSecond
+        document.getElementById('ovenCost').innerHTML = oven[2]
+        document.getElementById('ovens').innerHTML = oven[0]
+        document.getElementById('cps').innerHTML = scorePerSecond.toFixed(2)
         document.getElementById('cpc').innerHTML = clickingPower.toFixed(2)
-
+        if (ovenUpgrades[0] == 0 && oven[0] == 15 && i == 0) {
+            document.getElementById('upgrades').innerHTML += '<button onclick="ovenUpgrade()">Upgrade ovens: <br>Cost: <span id="ovenUpgradeCost">11000</span> </button>' +
+                '<span>Makes ovens twice as efficient</span>'
+            i = 1
+        }
     }
+
 }
 
 function buyFactory() {
-    if (score >= factoryCost) {
-        scorePerSecond += factoryValue * .5
-        clickingPowerMultiplier = (clickingPower / 15) + (factoryValue / 10)
-        clickingPower = clickingPower + (clickingPowerMultiplier / 5)
-        amount = factoryCost
-        factorys = factorys + 1
-        score = score - factoryCost
-        factoryCost = Math.round(factoryCost * 1.22)
+    let i = 0
+    if (score >= factory[2]) {
+        scorePerSecond += factory[1]
+        factory[0] = factory[0] + 1
+        score = score - factory[2]
+        factory[2] = Math.round(factory[2] * 1.14)
         document.getElementById('score').innerHTML = score.toFixed(2)
-        document.getElementById('factoryCost').innerHTML = factoryCost
-        document.getElementById('factorys').innerHTML = factorys
-        document.getElementById('cps').innerHTML = scorePerSecond
+        document.getElementById('factoryCost').innerHTML = factory[2]
+        document.getElementById('factorys').innerHTML = factory[0]
+        document.getElementById('cps').innerHTML = scorePerSecond.toFixed(2)
         document.getElementById('cpc').innerHTML = clickingPower.toFixed(2)
+        if (factoryUpgrades[0] == 0 && factory[0] == 15 && i == 0) {
+            document.getElementById('upgrades').innerHTML += '<button onclick="factoryUpgrade()">Upgrade factories: <br>Cost: <span id="factoryUpgradeCost">120000</span> </button>' +
+                '<span>Makes factories twice as efficient</span>'
+            i = 1
+        }
+    }
 
+
+}
+
+function cursorUpgrade() {
+    if (score > cursorUpgrades[2]) {
+        scorePerSecond -= (cursor[1] * cursor[0])
+        score = score - cursorUpgrades[2]
+        cursorUpgrades[0] += 1
+        cursorUpgrades[1] = cursorUpgrades[1] * 2
+        cursorUpgrades[2] = cursorUpgrades[2] * 5
+        clickingPower = clickingPower * 2
+        cursor[1] = cursor[1] + cursor[1]
+        scorePerSecond += cursor[1] * cursor[0]
+        document.getElementById('cursorUpgradeCost').innerHTML = cursorUpgrades[2].toFixed(2)
+        document.getElementById('score').innerHTML = score.toFixed(2)
+        document.getElementById('cps').innerHTML = scorePerSecond.toFixed(2)
+        document.getElementById('cpc').innerHTML = clickingPower.toFixed(2)
     }
 }
+
+function grannyUpgrade() {
+    if (score > grannyUpgrades[2]) {
+        scorePerSecond -= (granny[1] * granny[0])
+        score = score - grannyUpgrades[2]
+        grannyUpgrades[0] += 1
+        grannyUpgrades[1] = grannyUpgrades[1] * 2
+        grannyUpgrades[2] = grannyUpgrades[2] * 5
+        granny[1] = granny[1] + granny[1]
+        scorePerSecond += granny[1] * granny[0]
+        document.getElementById('grannyUpgradeCost').innerHTML = grannyUpgrades[2].toFixed(2)
+        document.getElementById('score').innerHTML = score.toFixed(2)
+        document.getElementById('cps').innerHTML = scorePerSecond.toFixed(2)
+        document.getElementById('cpc').innerHTML = clickingPower.toFixed(2)
+    }
+}
+
+function ovenUpgrade() {
+    if (score > ovenUpgrades[2]) {
+        scorePerSecond -= (oven[1] * oven[0])
+        score = score - ovenUpgrades[2]
+        ovenUpgrades[0] += 1
+        ovenUpgrades[1] = ovenUpgrades[1] * 2
+        ovenUpgrades[2] = ovenUpgrades[2] * 5
+        oven[1] = oven[1] * 2
+        scorePerSecond += (oven[1] * oven[0])
+        document.getElementById('ovenUpgradeCost').innerHTML = ovenUpgrades[2].toFixed(2)
+        document.getElementById('score').innerHTML = score.toFixed(2)
+        document.getElementById('cps').innerHTML = scorePerSecond.toFixed(2)
+        document.getElementById('cpc').innerHTML = clickingPower.toFixed(2)
+    }
+}
+
+function factoryUpgrade() {
+    if (score > factoryUpgrades[2]) {
+        scorePerSecond -= (factory[1] * factory[0])
+        score = score - factoryUpgrades[2]
+        factoryUpgrades[0] += 1
+        factoryUpgrades[1] = factoryUpgrades[1] * 2
+        factoryUpgrades[2] = factoryUpgrades[2] * 5
+        factory[1] = factory[1] * 2
+        scorePerSecond += (factory[1] * factory[0])
+        document.getElementById('factoryUpgradeCost').innerHTML = factoryUpgrades[2].toFixed(2)
+        document.getElementById('score').innerHTML = score.toFixed(2)
+        document.getElementById('cps').innerHTML = scorePerSecond.toFixed(2)
+        document.getElementById('cpc').innerHTML = clickingPower.toFixed(2)
+    }
+}
+
+function saveGame() {
+    var gameSave = {
+        score: score,
+        scorePerSecond: scorePerSecond,
+        clickingPower: clickingPower,
+        clickingPowerMultiplier: clickingPower,
+        cursor: cursor,
+        granny: granny,
+        oven: oven,
+        factory: factory,
+        cursorUpgrades: cursorUpgrades,
+        grannyUpgrades: grannyUpgrades,
+        ovenUpgrades: ovenUpgrades,
+        factoryUpgrades: factoryUpgrades
+    };
+    localStorage.setItem('gameSave', JSON.stringify(gameSave));
+}
+
+function loadGame() {
+    var savedGame = JSON.parse(localStorage.getItem('gameSave'));
+    if (typeof savedGame.score !== "undefined") score = savedGame.score
+    if (typeof savedGame.scorePerSecond !== "undefined") scorePerSecond = savedGame.scorePerSecond
+    if (typeof savedGame.clickingPower !== "undefined") clickingPower = savedGame.clickingPower
+    if (typeof savedGame.clickingPowerMultiplier !== "undefined") clickingPowerMultiplier = savedGame.clickingPowerMultiplier
+    if (typeof savedGame.cursor !== "undefined") cursor = savedGame.cursor
+    if (typeof savedGame.granny !== "undefined") granny = savedGame.granny
+    if (typeof savedGame.oven !== "undefined") oven = savedGame.oven
+    if (typeof savedGame.factory !== "undefined") factory = savedGame.factory
+    if (typeof savedGame.cursorUpgrades !== "undefined") cursorUpgrades = savedGame.cursorUpgrades
+    if (typeof savedGame.grannyUpgrades !== "undefined") grannyUpgrades = savedGame.grannyUpgrades
+    if (typeof savedGame.ovenUpgrades !== "undefined") ovenUpgrades = savedGame.ovenUpgrades
+    if (typeof savedGame.factoryUpgrades !== "undefined") factoryUpgrades = savedGame.factoryUpgrades
+}
+window.onload = function() {
+    loadGame();
+    if (cursorUpgrades[0] == 0 && cursor[0] >= 15) {
+        document.getElementById('upgrades').innerHTML += '<button onclick="cursorUpgrade()">Upgrade cursors: <br>Cost: <span id="cursorUpgradeCost">100</span> </button>' +
+            '<span>Makes cursors and mouseclicks twice as efficient</span>'
+    }
+    if (grannyUpgrades[0] == 0 && granny[0] >= 15) {
+        document.getElementById('upgrades').innerHTML += '<button onclick="grannyUpgrade()">Upgrade grannies: <br>Cost: <span id="grannyUpgradeCost">5000</span> </button>' +
+            '<span>Makes grannies twice as efficient</span>'
+    }
+    if (ovenUpgrades[0] == 0 && oven[0] >= 15) {
+        document.getElementById('upgrades').innerHTML += '<button onclick="ovenUpgrade()">Upgrade ovens: <br>Cost: <span id="ovenUpgradeCost">11000</span> </button>' +
+            '<span>Makes ovens twice as efficient</span>'
+    }
+    if (factoryUpgrades[0] == 0 && factory[0] >= 15) {
+        document.getElementById('upgrades').innerHTML += '<button onclick="factoryUpgrade()">Upgrade factories: <br>Cost: <span id="factoryUpgradeCost">120000</span> </button>' +
+            '<span>Makes factories twice as efficient</span>'
+    }
+
+    document.getElementById('score').innerHTML = score.toFixed(2)
+    document.getElementById('cursorCost').innerHTML = cursor[2]
+    document.getElementById('cursors').innerHTML = cursor[0]
+    document.getElementById('grannyCost').innerHTML = granny[2]
+    document.getElementById('grannys').innerHTML = granny[0]
+    document.getElementById('ovenCost').innerHTML = oven[2]
+    document.getElementById('ovens').innerHTML = oven[0]
+    document.getElementById('factoryCost').innerHTML = factory[2]
+    document.getElementById('factorys').innerHTML = factory[0]
+    document.getElementById('cps').innerHTML = scorePerSecond.toFixed(2)
+    document.getElementById('cpc').innerHTML = clickingPower.toFixed(2)
+
+}
+
+setInterval(function() {
+    saveGame();
+}, 30000)
+
+function resetGame() {
+    if (confirm('Are you sure you want to reset your game?')) {
+        var gameSave = {}
+        localStorage.setItem('gameSave', JSON.stringify(gameSave))
+        location.reload()
+    }
+}
+
+
 setInterval(function() {
     score = score + scorePerSecond
     document.getElementById('score').innerHTML = score.toFixed(2)
 
-}, 1000)
+}, 500)
+
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.which == 83) {
+        event.preventDefault()
+        saveGame()
+    }
+}, false)
